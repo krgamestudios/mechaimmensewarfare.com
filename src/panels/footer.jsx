f21509f69e2830e4be7e8b8aba1d4ce53c268c1c
Kayne <kayneruse@gmail.com>
2019-01-02 09:19:46 +1100

    Updated footer

--- src/panels/footer.jsx
+++ src/panels/footer.jsx
@@ -6,9 +6,10 @@ class Footer extends React.Component {
 	}
 
 	render() {
+		let year = new Date().getFullYear();
 		return (
 			<footer>
-				<p>Copyright <a href="http://krgamestudios.com">KR Game Studios</a> 2016-2018</p>
+				<p>Copyright <a href="http://krgamestudios.com">KR Game Studios</a> 2016-{year}</p>
 			</footer>
 		);
 	};
